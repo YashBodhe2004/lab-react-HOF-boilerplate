5b5b362f3708e8c787cf4217aef5b45c19a222b1
YashBodhe2004 <yashbodhe2004@gmail.com>
2024-01-08 18:05:16 +0530

    HOF

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HigherOrderComponents from './Component/HigherOrderComponents'
+
+function App() {
+  
+  return (
+    <div>
+      <HigherOrderComponents />
+    </div>
+  )
+}
+
+export default App
